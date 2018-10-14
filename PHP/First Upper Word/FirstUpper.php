ec31ae8d9239b9259d3c272a043f62d8d103d384
Reza Juliandri <rezajuliandri20@gmail.com>
2018-10-14 05:45:26 +0000

    Delete First Upper Word and move Age Generator

--- PHP/First Upper Word/FirstUpper.php
+++
@@ -1,9 +0,0 @@
-<?php
-function firstUpper($value){
-    $n = explode(" ",$value);
-    $str = "";
-    for ($i=0; $i < count($n); $i++) { 
-        $str .= ucfirst(strtolower($n[$i]))." ";
-    }
-    return trim($str);
-}
--- PHP/First Upper Word/example.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "FirstUpper.php";
-$string = "heLLo WorlD";
-echo $string."<br/>";
-$upper = firstUpper($string);
-echo $upper;
